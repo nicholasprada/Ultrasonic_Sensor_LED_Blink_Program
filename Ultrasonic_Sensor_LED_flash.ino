2a37172e63d137b6e0264945faa70cfcbf484283
nicholasprada <drprada073@gmail.com>
2025-10-06 14:35:56 -0400

    Update Ultrasonic_Sensor_LED_flash.ino

--- Ultrasonic_Sensor_LED_flash.ino
+++ Ultrasonic_Sensor_LED_flash.ino
@@ -21,28 +21,37 @@ void setup() {
 }
 
 void loop() {
+  // Turn off all output pins initially
   digitalWrite(led_pin, LOW);
   digitalWrite(trig, LOW);
   delay(2);
 
+  // Send a trigger pulse to the ultrasonic sensor
   digitalWrite(trig, HIGH);
   delay(10);
   digitalWrite(trig, LOW);
 
+  // Record the pulse from the echo pin and convert it to centimeters
   time_from_echo = pulseIn(echo, HIGH);
   distance_in_cm = time_from_echo * 0.017;
 
+  // Print the distance if connected to a computer
   Serial.print("Distance: ");
   Serial.print(distance_in_cm);
   Serial.print("cm");
 
+  // Keeps the LED on or off for 88ms
   delay(88);
+
+  // Checks the distance, and turns the LED on if it is less than 30cm
   if (distance_in_cm <= 30){
     digitalWrite(led_pin, HIGH);
   }
+  // Otherwise it ensures the LED off
   else{
     digitalWrite(led_pin, LOW);
   }
 
+  // Waits 100ms before repeating the loop
   delay(100);
 }
